c421f2e136d5571479c8f7ac4add383bbc74a2cb
Lee Howard <leegibsonhoward@gmail.com>
2021-01-15 22:35:37 -0800

    test(component): add test for welcome component
    - use react testing library and jest-dom for simple test example
    
    - component will probally never be used

---
+++ src/components/Welcome/Welcome.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import Welcome from './Welcome';
+
+test('has correct welcome text', () => {
+    render(<Welcome firstName="Jon" lastName="Snow" />)
+    expect(screen.getByRole('heading')).toHaveTextContent('Welcome, Jon Snow')
+  })
