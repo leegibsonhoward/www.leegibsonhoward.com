aeae006d73775d2daaabaeee9e6f943bd40fcad5
Lee Howard <leegibsonhoward@gmail.com>
2021-01-15 22:17:19 -0800

    chore(testing): install and configure jest
    - setup custom pre-process for babel-jest to play nice with docusaurus v2
    
    - install react testing library and jest-dom

---
+++ jest-preprocess.js
@@ -0,0 +1,7 @@
+const babelOptions = {
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react'
+    ],
+  }
+  module.exports = require("babel-jest").createTransformer(babelOptions)
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    transform: {
+      "^.+\\.(js|jsx)?$": `<rootDir>/jest-preprocess.js`,
+    },
+    testPathIgnorePatterns: [`node_modules`, `\\.docusaurus`, `<rootDir>/build`],
+    transformIgnorePatterns: [`node_modules/(?!(@babel)/)`],
+}
