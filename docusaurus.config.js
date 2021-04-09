e1ae94b7c0cf3fc71ecfcbded97aa15b8b2ce98c
Lee Howard <leegibsonhoward@gmail.com>
2021-04-09 15:53:44 -0700

    feat(footer): add my github handle
    - remove default docusaurus github url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/leegibsonhoward',
             },
           ],
         },
