64e12955a25a384ad931b159e7ee87eac16e2a88
Lee Howard <leegibsonhoward@gmail.com>
2021-04-08 18:28:21 -0700

    feat(pages): customize features section
    - add svg's for systems and games
    
    - remove existing svg's provided by docusaurus
    
    - add align-content to center for features module

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,32 +9,22 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: 'Systems Programming',
+    imageUrl: 'img/systems.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Unix/Linux environments, with a goal to achieve efficient use of
+        available resources.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: 'Game Development',
+    imageUrl: 'img/games.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        2D games using libraries. But also explore open source game engines such
+        such as Godette(Godot), and Unreal.
       </>
     ),
   },
@@ -43,7 +33,7 @@ const features = [
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx('col col--6', styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
