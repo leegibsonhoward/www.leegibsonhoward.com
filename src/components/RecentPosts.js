cfcfadc0cf2a496bd10e3b6b1d0a69a8fbffb7dc
Lee Howard <leegibsonhoward@gmail.com>
2021-04-14 17:44:09 -0700

    feat(components): add recent posts component
    - no styles and not in use

---
+++ src/components/RecentPosts.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import recentPosts from '../../.docusaurus/docusaurus-plugin-content-blog/default/blog-post-list-prop-default.json';
+
+/**
+ * Recent Posts Component
+ * @param {object} props Component props
+ * @param {number} props.posts Number of posts to show (1 = only most recent)
+ */
+
+function RecentPosts(props) {
+  return (
+    <ul>
+      {recentPosts.items.slice(0, props.posts).map((item, index) => (
+        <li key={index}>
+          <a href={`${item.permalink}`}>{item.title}</a>{' '}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+RecentPosts.propTypes = {
+  posts: PropTypes.number.isRequired,
+};
+
+export default RecentPosts;
