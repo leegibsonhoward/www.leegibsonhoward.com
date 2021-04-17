415cf37a02467e03362b62531e252ef5d8ac2945
Lee Howard <leegibsonhoward@gmail.com>
2021-04-16 20:44:45 -0700

    test(components): test RecentPosts header text
    - should render Recent Post

--- src/components/RecentPosts.test.js
+++ src/components/RecentPosts.test.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render, screen, within } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import RecentPosts from './RecentPosts';
+
+test('has correct header text', () => {
+  render(<RecentPosts posts={1} />);
+  expect(screen.getByRole('banner')).toHaveTextContent('Recent Post');
+});
 
 test('has one post in list', () => {
   render(<RecentPosts posts={1} />);
