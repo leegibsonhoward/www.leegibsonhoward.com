57d0f3399aaacf86d6149dbe15edc56ddd73a303
leegibsonhoward <leegibsonhoward@gmail.com>
2021-04-20 23:18:56 +0000

    Deploy website - based on b6e0e33560d29b8c0a915589ca3220a2cde97a82

---
+++ 3b83af36.93c79a9e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{77:function(o){o.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Docusaurus as My Blog Platform","permalink":"/blog/docusaurus-as-my-blog-platform"}]}')}}]);
